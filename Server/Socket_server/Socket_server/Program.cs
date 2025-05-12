b373cd65f44361ac9aa6556d04f999e6962ad0e1
Sean-mn <b01080080329@gmail.com>
2025-05-13 00:56:49 +0900

    서버 구현

--- Server/Socket_server/Socket_server/Program.cs
+++ Server/Socket_server/Socket_server/Program.cs
@@ -1,14 +1,30 @@
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
 namespace Socket_server;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
-        var app = builder.Build();
+        TcpListener server = new TcpListener(IPAddress.Any, 8888);
+        server.Start();
+        Console.WriteLine("서버 시작... 기다리는 중...");
+        
+        TcpClient client = server.AcceptTcpClient();
+        Console.WriteLine("클라이언트 연결됨!");
+        
+        NetworkStream stream = client.GetStream();
+        StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+        StreamWriter writer = new StreamWriter(stream, Encoding.UTF8){AutoFlush = true};
 
-        app.MapGet("/", () => "Hello World!");
-
-        app.Run();
+        while (true)
+        {
+            string msg = reader.ReadLine();
+            Console.WriteLine($"받은 메세지: {msg}");
+            
+            writer.WriteLine($"서버 응답: {msg.ToUpper()}");
+        }
     }
 }
